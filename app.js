df8ec0ba6acdc895d263123c6db0a6a1175e0c9b
Moon Jo <urwisdom77@gmail.com>
2019-12-22 19:49:16 -0500

    feat: Able to store train data in firebase and append into table body. Still have to figure out the minutes away.

--- app.js
+++ app.js
@@ -8,25 +8,51 @@ var firebaseConfig = {
   messagingSenderId: "88141595545",
   appId: "1:88141595545:web:55a4fc8a7d051514fa8908"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+//db ref
 const db = firebase.firestore();
+//html element ref
+
+var trainName = document.querySelector("#train-name-input").value;
+var trainDestination = document.querySelector("#destination-input").value;
+var trainFirst = document.querySelector("#first-train-input").value;
+var trainFrequency = document.querySelector("#frequency-input").value;
+var trainArr = [trainName, trainDestination, trainFirst, trainFrequency];
+
 $("#submit").on("click", function(event) {
   event.preventDefault();
+
+  var trainName = document.querySelector("#train-name-input").value;
+  var trainDestination = document.querySelector("#destination-input").value;
+  var trainFirst = document.querySelector("#first-train-input").value;
+  var trainFrequency = parseInt(
+    document.querySelector("#frequency-input").value
+  );
+  var trainArr = [trainName, trainDestination, trainFirst, trainFrequency];
+
+  console.log(trainArr);
+
+  //
   db.collection("trains").add({
-    name: $("#train-name")
-      .val()
-      .trim(),
-    destination: $("#destination")
-      .val()
-      .trim(),
-    firstTRain: $("#first-train")
-      .val()
-      .trim(),
-    frequency: parseInt(
-      $("#frequency")
-        .val()
-        .trim()
-    )
+    name: trainName,
+    destination: trainDestination,
+    firstTrain: trainFirst,
+    frequency: trainFrequency
   });
+});
+db.collection("trains").onSnapshot(function(snap) {
+  var trainsRows = snap.docs.map(function(doc) {
+    console.log(doc.data());
+    const { name, destination, firstTrain, frequency } = doc.data();
+    return `<tr>
+    <td>${name}</td>
+    <td>${destination}</td>
+    <td>${firstTrain}</td>
+    <td>${frequency}</td>
+    </tr>`;
+  });
+  console.log(trainsRows);
+  $("#table-body").append(trainsRows);
 });
